0d887418e8de339be1e0b448736d9477638495cf
Rodrigues <fr.costa95@gmail.com>
2022-07-23 01:54:47 -0300

    feat(css, component): improve list component
    	Included new classes to customize app in app.css.
    	Improved list component, use chip component of material-ui.
    	Added treatment if pokemon not found.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,31 +1,77 @@
 import React, { useState } from 'react';
 import { SearchForm } from './SearchForm';
 import { getPokemon } from '../services/pokemon';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, Card, Divider, Chip } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+  },
+}));
 
 export const List = () => {
+  const classes = useStyles();
   const [abilities, setAbilities] = useState([])
+  const [error, setError] = useState('')
 
   const handleSearch = async name => {
     getPokemon(name)
       .then(res => {
+        setError('')
         setAbilities(res.data)
       }).catch(err => {
         console.log('err', err)
+        debugger
+        setAbilities([])
+        setError(err.response.data.message)
       });
   }
 
   return (
     <>
-      <AppBar>
+      <AppBar color='secondary'>
         <Toolbar className='toolbar'>
           <h1>Search a pokemon abilities by name</h1>
         </Toolbar>
       </AppBar>
       <SearchForm handleSearch={handleSearch} />
-      <div>
-        {abilities}
-      </div>
+      {
+        (abilities.length > 0 || error !== '') &&
+        <Card className='card-abilities' variant='outlined'>
+          PokemonName
+          <Divider />
+          {
+            error === '' &&
+            <>
+              <h4>
+                Powers:
+              </h4>
+              <div className={classes.root}>
+                {
+                  abilities.map((a, i) => {
+                    return (
+                      <Chip
+                        className=''
+                        key={i}
+                        label={a}
+                        clickable
+                        color="secondary"
+                      />
+                    )
+                  })
+                }
+              </div>
+            </>
+          }
+          { error !== '' && error }
+        </Card>
+      }
     </>
   )
 }
